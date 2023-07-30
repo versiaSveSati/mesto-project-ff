f4af14ad40c4c8ad290080d3f8c244865486b914
Svetlana Levadnaya <sv_levadnaya@mail.ru>
2023-07-30 17:03:41 +0300

    refactor: форматировать документ

--- scripts/index.js
+++ scripts/index.js
@@ -16,17 +16,17 @@ let popupCloseButtonElement = popupElement.querySelector('.popup__close');
 // console.log(popupCloseButtonElement);
 
 function togglePopup() {
-    console.log('togglePopup');
-    popupElement.classList.toggle('popup_is-opened');
-    // toggle
-    // если класс навешен - убери его
-    // если класса нет - добавь его
-  }
-  //присваиваю значение функции togglePopup
-  popupOpenButtonElement.addEventListener('click', togglePopup);
-  popupCloseButtonElement.addEventListener('click', togglePopup);
+  console.log('togglePopup');
+  popupElement.classList.toggle('popup_is-opened');
+  // toggle
+  // если класс навешен - убери его
+  // если класса нет - добавь его
+}
+//присваиваю значение функции togglePopup
+popupOpenButtonElement.addEventListener('click', togglePopup);
+popupCloseButtonElement.addEventListener('click', togglePopup);
 
-  // Находим форму в DOM
+// Находим форму в DOM
 let formElement = popupElement.querySelector('.popup__container');
 // Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__form_name');
@@ -34,22 +34,22 @@ let jobInput = formElement.querySelector('.popup__form_job');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    const profileName = document.querySelector(".profile__name");
-    const profileText = document.querySelector(".profile__text");
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameInput.value;
-    profileText.textContent = jobInput.value;
+function handleFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+  const profileName = document.querySelector(".profile__name");
+  const profileText = document.querySelector(".profile__text");
+  // Вставьте новые значения с помощью textContent
+  profileName.textContent = nameInput.value;
+  profileText.textContent = jobInput.value;
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
 
 // ищем внутри попапа кнопку сохранить
 let popupSave = formElement.querySelector('.popup__save');
