5b44ef1e574051e1e1c06a717eca8b170d4e6729
Svetlana Levadnaya <sv_levadnaya@mail.ru>
2023-07-31 22:16:30 +0300

    fix: исправить js, на ревью2

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,53 @@
-//TODO 1. Проверяем что подключили скрипт и он работает
-console.log('Hello');
-
-//TODO 2. Делаем выборку DOM элементов
-// найдем нашу кнопку открытия модального окна
-let popupOpenButtonElement = document.querySelector('.profile__pencil');
-// console.log(popupOpenButtonElement);
-
 // найдем модальное окно
 let popupElement = document.querySelector('.popup');
-// console.log(popupElement);
-
-// кнопка закрытия модального окна
-// ищем ВНУТРИ попапа
+// найдем кнопку открытия модального окна
+let popupOpenButtonElement = document.querySelector('.profile__pencil');
+// найдем кнопку закрытия модального окна, ищем ВНУТРИ попапа
 let popupCloseButtonElement = popupElement.querySelector('.popup__close');
-// console.log(popupCloseButtonElement);
 
-function togglePopup() {
-  console.log('togglePopup');
-  popupElement.classList.toggle('popup_opened');
-  // toggle
-  // если класс навешен - убери его
-  // если класса нет - добавь его
+
+//функция открытия 
+function popupOpen() {
+  popupElement.classList.add('popup_opened');
 }
-//присваиваю значение функции togglePopup
-popupOpenButtonElement.addEventListener('click', togglePopup);
-popupCloseButtonElement.addEventListener('click', togglePopup);
+//присваиваю значение функции
+popupOpenButtonElement.addEventListener('click', popupOpen);
 
-// Находим форму в DOM
-let formElement = popupElement.querySelector('.popup__container');
-// Находим поля формы в DOM
+//функция закрытия 
+function popupClose() {
+  popupElement.classList.remove('popup_opened');
+}
+//присваиваю значение функции
+popupCloseButtonElement.addEventListener('click', popupClose);
+
+
+//Находим форму
+let formElement = popupElement.querySelector('.popup__forms');
+// Выбираем элементы, куда должны быть вставлены значения полей
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
+// Находим поля формы
 let nameInput = formElement.querySelector('.popup__form_type_name');
 let jobInput = formElement.querySelector('.popup__form_type_job');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+
+
+
+
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+  // Эта строчка отменяет стандартную отправку формы, так мы можем определить свою логику отправки.
+  evt.preventDefault();
   // Получите значение полей jobInput и nameInput из свойства value
-  // Выберите элементы, куда должны быть вставлены значения полей
-  const profileName = document.querySelector(".profile__name");
-  const profileText = document.querySelector(".profile__text");
   // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profileText.textContent = jobInput.value;
+  // вызываем функцию закрытия попапа
+  popupClose();
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
-// ищем внутри попапа кнопку сохранить
-let popupSave = formElement.querySelector('.popup__save');
-//присваиваю значение функции togglePopup
-popupSave.addEventListener('click', togglePopup);
+
 
