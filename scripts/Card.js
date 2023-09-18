7c0cb37a16d00c4ec92664b7fadd9e6d2e7163b1
Svetlana Levadnaya <sv_levadnaya@mail.ru>
2023-09-18 21:47:00 +0300

    fix: исправить Card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -15,8 +15,10 @@ export default class Card {
 
     createCard() { //создать карточку
         this._element = this._getTemplate();
+        this._buttonLike = this._element.querySelector('.card__like'); //нашли кнопку лайка
         this._element.querySelector('.card__photo').src = this._link; //ссылка на картинку
-        this._element.querySelector('.card__photo').alt = this._name; //картинка
+        this._cardImage = this._element.querySelector('.card__photo');// нашли картинку
+        this._cardImage.alt = this._name; //картинка
         this._element.querySelector('.card__text').textContent = this._name; //название 
         this._setEventListener();
         return this._element;
@@ -26,19 +28,20 @@ export default class Card {
         this._element.querySelector('.card__button').addEventListener('click', () => { //кнопка удалить
             this._handleCardTrash();
         });
-        this._element.querySelector('.card__like').addEventListener('click', (event) => { //кнопка сердечко
+        this._buttonLike.addEventListener('click', (event) => { //кнопка сердечко
             this._handleCardLike(event);
         });
-        this._element.querySelector('.card__photo').addEventListener('click', () => { //картинка
+        this._cardImage.addEventListener('click', () => { //картинка
             this._handleOpenCard(this._link, this._name);
         });
     }
 
     _handleCardTrash() {
         this._element.remove();
+        this._element = null;
     }
 
     _handleCardLike(event) {
-        event.target.classList.toggle('card__like_active');
+        this._buttonLike.classList.toggle('card__like_active');
     }
 }
