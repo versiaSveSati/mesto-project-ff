5eb02a9a0a8d96fd3e3b962b8eb04d819bda83cf
Svetlana Levadnaya <sv_levadnaya@mail.ru>
2023-10-30 18:06:04 +0300

    feat: API2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,26 @@
 
 const configForm = {
-    formSelector: ".popup__form",
-    inputSelector: ".popup__form-item",
-    submitButtonSelector: ".popup__form-button",
-    inactiveButtonClass: "popup__form-button_inactive",
-    inputErrorClass: "popup__input_error",
-    formItemErrorClass: "popup__form-item_error",
-  };
-  
-  const forms = document.querySelectorAll(".popup__form");
-  //попап с профилем
-  const popupProfile = document.querySelector("#profile");
-  const editButton = document.querySelector(".profile__edit-button");
-  //попап с добавлением картинок
-  const addPictureButton = document.querySelector(".profile__add-button");
-  //находим форму для имени и работы
-  const profileFormElement = popupProfile.querySelector(".popup__form");
-  const nameInput = profileFormElement.querySelector("#name");
-  const jobInput = profileFormElement.querySelector("#job");
-  
-  //кнопка для аватара
-  const avatarButton = document.querySelector('.profile__user-avatar-button');
-  
-  
-  export { configForm, forms, editButton, addPictureButton, nameInput, jobInput, avatarButton };
+    formSelector: ".popup__forms",
+    inputSelector: ".popup__form",
+    submitButtonSelector: ".popup__save",
+    inactiveButtonClass: "popup__save_type_invalid",
+    inputErrorClass: "popup__form_type_invalid",
+    formItemErrorClass: "popup__error",
+};
+
+const forms = document.querySelectorAll(".popup__forms");
+//попап с профилем
+const popupProfile = document.querySelector("#profile");
+const editButton = document.querySelector(".profile__pencil");
+//попап с добавлением картинок
+const addPictureButton = document.querySelector(".profile__plus");
+//находим форму для имени и работы
+const profileFormElement = popupProfile.querySelector(".popup__forms");
+const nameInput = profileFormElement.querySelector("#name");
+const jobInput = profileFormElement.querySelector("#profession");
+
+//кнопка для аватара
+const avatarButton = document.querySelector('.profile__avatar-button');
+
+
+export { configForm, forms, editButton, addPictureButton, nameInput, jobInput, avatarButton };
